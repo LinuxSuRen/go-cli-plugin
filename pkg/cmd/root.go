ab0e910e592f6987ec57488fd786677967e440c9
rick <rick@jenkins-zh.cn>
2021-01-27 17:50:55 +0800

    Fix the plugin loading

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -6,7 +6,7 @@ import (
 
 // AppendPluginCmd create a command as root of config plugin
 func AppendPluginCmd(root *cobra.Command, pluginOrg, pluginRepo string) {
-	root.AddCommand(NewConfigPluginListCmd(),
+	root.AddCommand(NewConfigPluginListCmd(pluginOrg, pluginRepo),
 		NewConfigPluginFetchCmd(pluginOrg, pluginRepo),
 		NewConfigPluginInstallCmd(pluginOrg, pluginRepo),
 		NewConfigPluginUninstallCmd(pluginOrg, pluginRepo))
